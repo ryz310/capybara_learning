82372b84cda4da39d091294265e82e001389b5d7
Ryosuke Sato <ryz310@gmail.com>
2014-03-15 16:17:49 +0900

    未実装の機能を実装

--- spec/models/phone_spec.rb
+++ spec/models/phone_spec.rb
@@ -4,16 +4,16 @@ describe Phone do
   it 'does not allow duplicate phone numbers per contact' do
     contact = create(:contact)
     contact.phones.create(phone_type: 'home',   phone: '090-1234-5678')
-    contact.phones.create(phone_type: 'mobile', phone: '090-1234-5678')
-    expect(contact).to have(1).errors_on(:phone)
+    mobile_phone = contact.phones.create(phone_type: 'mobile', phone: '090-1234-5678')
+    expect(mobile_phone).to have(1).errors_on(:phone)
   end
 
   it 'allows two contacts to share a phone number' do
     contact = create(:contact)
     contact.phones.create(phone_type: 'home', phone: '090-1234-5678')
-    other_contact = create(:contact)
-    other_contact.phones.build(phone_type: 'home', phone: '090-1234-5678')
+    other_contact = create(:contact, email: 'other@email.com')
+    other_phone = other_contact.phones.build(phone_type: 'home', phone: '090-1234-5678')
 
-    expect.(other_contact).to be_valid
+    expect(other_phone).to be_valid
   end
 end
