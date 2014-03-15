4f5ff274dff3e5726e01208d24ae9cd6cfa3cab6
Ryosuke Sato <ryz310@gmail.com>
2014-03-15 15:42:09 +0900

    model Contact を作成し、テストを追加

---
+++ spec/models/contact_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe Contact do
+  it 'is valid with a firstname, lastname, and email' do
+    expect(build(:contact)).to be_valid
+  end
+
+  it 'is invalid without a fitstname' do
+    expect(build(:contact, firstname: nil)).to have(1).errors_on(:firstname)
+  end
+
+  it 'is invalid without a lastname'
+  it 'is invalid without an email address'
+  it 'is invalid with a duplicate email address' do
+    create(:contact, email: 'example@test.com')
+    contact = create(:contact, email: 'example@test.com')
+    expect(contact).to have(1).errors_on(:email)
+  end
+
+  it "returns a contact's full name as a string"
+
+end
