c0403646a8f18a5895ef7f0b76f2678c33182388
Ryosuke Sato <ryz310@gmail.com>
2014-03-21 12:24:35 +0900

    Userの新規作成のfeaturesテストを作成

---
+++ spec/features/users_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+feature 'User Management' do
+  scenario 'add a new user' do
+    visit root_path
+    expect{
+      click_link 'New User'
+      fill_in 'Name', with: 'user name'
+      fill_in 'Password', with: 'password'
+      fill_in 'Email', with: 'test@mail.com'
+      click_button 'Create User'
+    }.to change(User, :count).by(1)
+  end
+end
