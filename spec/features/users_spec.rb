0ba715d9d68e43d7bf546112823a9396628a30ab
Ryosuke Sato <ryz310@gmail.com>
2014-03-21 12:39:44 +0900

    scenario 'xxx' js: true に対応。

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 feature 'User Management' do
-  scenario 'add a new user' do
+  scenario 'add a new user', js: true do
     visit root_path
     expect{
       click_link 'New User'
