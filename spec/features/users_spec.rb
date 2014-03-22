8da037840bd18a71cd3c6d9598a171dd08c40511
Ryosuke Sato <ryz310@gmail.com>
2014-03-22 21:59:12 +0900

    show, edit, delete についてもテストを実装。

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -1,8 +1,12 @@
 require 'spec_helper'
 
-feature 'User Management' do
-  scenario 'add a new user', js: true do
+feature 'User Management', js: true do
+  background do
+    create(:user, name: 'John', email: 'hoge@mail.com')
     visit root_path
+  end
+
+  scenario 'add a new user' do
     expect{
       click_link 'New User'
       fill_in 'Name', with: 'user name'
@@ -10,5 +14,23 @@ feature 'User Management' do
       fill_in 'Email', with: 'test@mail.com'
       click_button 'Create User'
     }.to change(User, :count).by(1)
+  end
+
+  scenario 'show user list' do
+    click_link 'Show'
+    expect(page).to have_content 'John'
+  end
+
+  scenario 'edit user name' do
+    click_link 'Edit'
+    fill_in 'Name', with: 'Jack'
+    click_button 'Update User'
+    expect(page).to have_content 'Jack'
+  end
+
+  scenario 'delete user' do
+    expect{
+      click_link 'Destroy'
+    }.to change(User, :count).by(-1)
   end
 end
